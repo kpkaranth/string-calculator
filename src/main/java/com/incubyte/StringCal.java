1b59fac94e61424011a057a7a20df09f881545e7
Prashanth <prashanth@iclinica.in>
2024-08-06 18:21:37 +0530

    Refactored to make things simpler

--- src/main/java/com/incubyte/StringCal.java
+++ src/main/java/com/incubyte/StringCal.java
@@ -11,14 +11,14 @@ public class StringCal {
             return 0;
         }
 
-        String delimiter = ",\n";
+        String delimiter = ",|\n";
         if (numbersStr.startsWith("//")) {
             int index = numbersStr.indexOf("\n");
-            delimiter = numbersStr.substring(2, index) + "\n";
+            delimiter = numbersStr.substring(2, index) + "|\n";
             numbersStr = numbersStr.substring(index + 1);
         }
 
-        String[] arrNumber = getNumbers(numbersStr, delimiter);
+        String[] arrNumber = numbersStr.split(delimiter);
         int sum = 0;
         List<Integer> negativeNumbers = new ArrayList<>();
         for (String numberStr : arrNumber) {
@@ -38,25 +38,5 @@ public class StringCal {
         }
 
         return sum;
-    }
-
-    //    TODO: Explore if there is better way to do things
-    public String[] getNumbers(String numbersStr, String delimiterStr) {
-        String[] delimiters = delimiterStr.split("");
-        List<String> list = new ArrayList<>();
-
-        //Split by ',' or any other delimiter
-        String[] arrNumber1 = numbersStr.split(delimiters[0]);
-        if (delimiters.length == 2) {
-            for (String numbersStr2 : arrNumber1) {
-                //Split by '\n'
-                String[] arrNumber2 = numbersStr2.split(delimiters[1]);
-                list.addAll(new ArrayList<>(Arrays.asList(arrNumber2)));
-            }
-        } else {
-            list = new ArrayList<>(new ArrayList<>(Arrays.asList(arrNumber1)));
-        }
-
-        return list.toArray(new String[0]);
     }
 }
